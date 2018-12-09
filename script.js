be2be4f60fde5576ff62bb811bf5039caadea293
Ken Girard <ken.girard@gmail.com>
2018-12-08 22:59:20 -0600

    Added in function setPositionForWeatherInfo

--- script.js
+++ script.js
@@ -63,6 +63,19 @@ function init(resultFromServer) {
     windSpeedElement.innerHTML = 'Winds at ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
     cityHeader.innerHTML = resultFromServer.name;
     humidityElement.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity + '%';
+
+    setPositionForWeatherInfo();
+}
+
+function setPositionForWeatherInfo() {
+    let weatherContainer = document.getElementById('weatherContainer');
+    let weatherContainerHeight = weatherContainer.clientHeight;
+    let weatherContainerWidth = weatherContainer.clientWidth;
+
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
+    weatherContainer.style.visibility = `visible`;
+    
 }
 
 document.getElementById('searchBtn').addEventListener('click', () => {
