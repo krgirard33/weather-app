69cd6a56f497e7a13d879db5f431f7f159459937
Ken Girard <ken.girard@gmail.com>
2018-12-08 18:08:14 -0600

    Initial commit, zip search works

---
+++ script.js
@@ -0,0 +1,21 @@
+let appId = '4c5cfc087f4a77f084cb19093a02bef7';
+let units = 'imperial';
+let searchMethod = 'zip';
+
+function searchWeather(searchTerm) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+        return result.json();
+    }).then(result => {
+        init(result);
+    })
+}
+
+function init(resultFromServer) {
+    console.log(resultFromServer);
+}
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm)
+        searchWeather(searchTerm);
+})
